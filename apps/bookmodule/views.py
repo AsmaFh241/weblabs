5aa4912fa95f15ba288ec75028e2f0fdc8725062
Asma <Asmafh555@gmail.com>
2026-02-23 20:41:20 +0300

    Completed Lab 3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render 
+def index(request):
+    return render(request, 'bookmodule/index.html')
+def viewbook(request, bookId):
+    
+    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble'}
+    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    
+    targetBook = None
+    if book1['id'] == bookId: targetBook = book1
+    if book2['id'] == bookId: targetBook = book2
+
+    
+    return render(request, 'bookmodule/show.html', {'book': targetBook})
